a33e9d67352a527d6e0725281278561cfa479ba1
Pu Chen <puchen.tw@gmail.com>
2022-11-22 10:47:36 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0de3de6191bdf04b6885.js"
+    "url": "webpack-runtime-7d33929c74f779f5f964.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "254ae3abc820e42a0f3505ff2bd715bc"
+    "revision": "1e4bd04b4ab96f3c75401bc617cffc37"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "37d306828cd64f63afac05cea887fbbb"
+    "revision": "0716e5eb4daa6ace414edf376fcba926"
   },
   {
     "url": "polyfill-8296ab92a56f464716e4.js"
